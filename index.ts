24bf5e948cde00d0139c20bfa9ecf6d788ea87f9
Juan Campa <juancampa@gmail.com>
2023-09-11 13:51:41 -0400

    Args codemod

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-import { root, nodes, state } from "membrane";
+import { nodes } from "membrane";
 const { sys_email } = nodes;
 
 export const Root = {
-  configure: async ({ args }) =>
-    await sys_email.configure({ ...args }).$invoke(),
+  configure: async (args) => await sys_email.configure({ ...args }).$invoke(),
 
-  send: async ({ args }) => {
+  send: async (args) => {
     await sys_email.send({ ...args }).$invoke();
   },
 };
