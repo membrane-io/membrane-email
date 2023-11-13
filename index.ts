cc2dd36a715a31b4925e2085aff8c9a224338da5
Jhonny <aranajhonny@users.noreply.github.com>
2023-11-13 11:25:30 -0500

    delete configure

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ import { nodes } from "membrane";
 const { sys_email } = nodes;
 
 export const Root = {
-  configure: async (args) => await sys_email.configure({ ...args }).$invoke(),
-
   send: async (args) => {
     await sys_email.send({ ...args }).$invoke();
   },
